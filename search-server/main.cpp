77129d0867f2f64e45520af90fb5f8b0ffe34015
Xottabich77 <samoshkin-max@yandex.ru>
2023-05-07 15:55:30 +0600

    sprint 1

--- search-server/main.cpp
+++ search-server/main.cpp
@@ -1,18 +1,155 @@
-// Решите загадку: Сколько чисел от 1 до 1000 содержат как минимум одну цифру 3?
-// Напишите ответ здесь:
+#include <algorithm>
 #include <iostream>
+#include <set>
 #include <string>
+#include <utility>
+#include <vector>
+#include <map>
+#include <cmath>
 
 using namespace std;
 
-int main(){
-string num;
-int k=0;
-for(int i=1; i<1001;++i){
-	num=to_string(i);
-	for(char c:num) if(c=='3') ++k;
-	}
-cout <<"В ряде от 1 до 1000 - "s <<k <<" чисел содержат хотябы одну цифру 3"s <<endl;
+const int MAX_RESULT_DOCUMENT_COUNT = 5;
+
+string ReadLine() {
+    string s;
+    getline(cin, s);
+    return s;
 }
 
-// Закомитьте изменения и отправьте их в свой репозиторий.
+int ReadLineWithNumber() {
+    int result = 0;
+    cin >> result;
+    ReadLine();
+    return result;
+}
+
+vector<string> SplitIntoWords(const string& text) {
+    vector<string> words;
+    string word;
+    for (const char c : text) {
+        if (c == ' ') {
+            if (!word.empty()) {
+                words.push_back(word);
+                word.clear();
+            }
+        } else {
+            word += c;
+        }
+    }
+    if (!word.empty()) {
+        words.push_back(word);
+    }
+
+    return words;
+}
+
+struct Document {
+    int id;
+    double relevance;
+};
+
+class SearchServer {
+public:
+    void SetStopWords(const string& text) {
+        for (const string& word : SplitIntoWords(text)) {
+            stop_words_.insert(word);
+        }
+    }
+
+    void AddDocument(int document_id, const string& document) {
+      const vector<string> words = SplitIntoWordsNoStop(document);
+      ++document_count_;
+      double tf_for_word=1/double(words.size());
+      for(string word:words) documents_[word][document_id]+=tf_for_word;
+    }
+
+    vector<Document> FindTopDocuments(const string& raw_query) const {
+        const set<string> query_words = ParseQuery(raw_query);
+        auto matched_documents = FindAllDocuments(query_words);
+
+        sort(matched_documents.begin(), matched_documents.end(),
+             [](const Document& lhs, const Document& rhs) {
+                 return lhs.relevance > rhs.relevance;
+             });
+        if (matched_documents.size() > MAX_RESULT_DOCUMENT_COUNT) {
+            matched_documents.resize(MAX_RESULT_DOCUMENT_COUNT);
+        }
+        return matched_documents;
+    }
+
+private:
+    
+    map<string, map<int,double>> documents_;
+    int document_count_ = 0;
+    set<string> stop_words_;
+
+    bool IsStopWord(const string& word) const {
+        return stop_words_.count(word) > 0;
+    }
+
+    vector<string> SplitIntoWordsNoStop(const string& text) const {
+        vector<string> words;
+        for (const string& word : SplitIntoWords(text)) {
+            if (!IsStopWord(word)) {
+                words.push_back(word);
+            }
+        }
+        return words;
+    }
+
+    set<string> ParseQuery(const string& text) const {
+        set<string> query_words;
+        for (const string& word : SplitIntoWordsNoStop(text)) {
+            query_words.insert(word);
+        }
+        return query_words;
+    }
+
+    vector<Document> FindAllDocuments(const set<string>& query_words) const {
+        vector<Document> matched_documents;
+        map <int,double> index_doc;
+        double idf_word;
+        string minus_word;
+       // ищем по слову есть ли оно в документах
+      for(auto word:query_words){
+            minus_word='-'+word;
+            if(documents_.count(minus_word)!=0){
+                for(const auto [id,rl]:documents_.at(word)) index_doc.erase(id);
+                 continue;}
+            if(documents_.count(word)!=0){
+               idf_word = log(double(document_count_) / double(documents_.at(word).size()));
+               for(const auto [id,rl]:documents_.at(word)) index_doc[id]+=rl*idf_word;
+               }    
+        }
+    // переносим найденые документы    
+       for(const auto& [key,value]:index_doc)
+           matched_documents.push_back({key,value});
+              
+    return matched_documents;
+    }
+
+  
+};
+
+SearchServer CreateSearchServer() {
+    SearchServer search_server;
+    search_server.SetStopWords(ReadLine());
+
+    const int document_count = ReadLineWithNumber();
+    for (int document_id = 0; document_id < document_count; ++document_id) {
+        search_server.AddDocument(document_id, ReadLine());
+    }
+
+    return search_server;
+}
+
+int main() {
+    const SearchServer search_server = CreateSearchServer();
+
+    const string query = ReadLine();
+    for (const auto& [document_id, relevance] : search_server.FindTopDocuments(query)) {
+        cout << "{ document_id = "s << document_id << ", "
+             << "relevance = "s << relevance << " }"s << endl;
+    }
+}
