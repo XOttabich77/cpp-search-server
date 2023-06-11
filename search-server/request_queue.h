3d28624df9d98a6707e588f3ac22384e42b6fe2e
Maxim Samoshkin <Samoshkin-Max@yandex.ru>
2023-06-11 13:17:12 +0600

    sprin5 m2

--- search-server/request_queue.h
+++ search-server/request_queue.h
@@ -12,11 +12,7 @@ public:
     explicit RequestQueue(const SearchServer& search_server);
  
     template <typename DocumentPredicate>
-    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate) {
-        auto documents = search_server_.FindTopDocuments(raw_query, document_predicate);
-        AddResultRequest(documents.size());
-        return documents;
-    }
+    std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate);
     std::vector<Document> AddFindRequest(const std::string& raw_query, DocumentStatus status);
     std::vector<Document> AddFindRequest(const std::string& raw_query);
     
@@ -36,3 +32,10 @@ private:
     void AddResultRequest(int num);
 };
 
+template<typename DocumentPredicate>
+inline std::vector<Document> RequestQueue::AddFindRequest(const std::string& raw_query, DocumentPredicate document_predicate)
+{
+    auto documents = search_server_.FindTopDocuments(raw_query, document_predicate);
+    AddResultRequest(documents.size());
+    return documents;
+}
